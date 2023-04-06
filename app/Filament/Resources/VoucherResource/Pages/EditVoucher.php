2678a4adadaa76529ab24911f60fb493ae614238
Vedat Kurtay <vedatkurtay@gmail.com>
2023-04-06 16:58:02 +0300

    Added Restricting edit on Some condition of the record

--- app/Filament/Resources/VoucherResource/Pages/EditVoucher.php
+++ app/Filament/Resources/VoucherResource/Pages/EditVoucher.php
@@ -10,6 +10,14 @@ class EditVoucher extends EditRecord
 {
     protected static string $resource = VoucherResource::class;
 
+    protected function beforeFill()
+    {
+        if ($this->record->payments()->exists()){
+            $this->notify('danger', 'This voucher has been used and cannot be edited.');
+            $this->redirect($this->getResource()::getUrl('index'));
+        }
+    }
+
     protected function getActions(): array
     {
         return [
