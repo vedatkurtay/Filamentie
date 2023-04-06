b232281ef35fd7a15cfdbeb454d5382c002ee042
Vedat Kurtay <vedatkurtay@gmail.com>
2023-04-06 14:57:28 +0300

    Added image column for table and forms

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -33,6 +33,7 @@ class ProductResource extends Resource
                     }),
                 Forms\Components\TextInput::make('slug')->required(),
                 Forms\Components\TextInput::make('price')->required()->rules('numeric'),
+                Forms\Components\FileUpload::make('image')->image(),
             ]);
     }
 
@@ -40,6 +41,7 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
+                Tables\Columns\ImageColumn::make('image')->width(50)->height(50),
                 Tables\Columns\TextColumn::make('name')->sortable()->searchable(),
                 Tables\Columns\TextColumn::make('price')->sortable()->money('try'),
             ])
