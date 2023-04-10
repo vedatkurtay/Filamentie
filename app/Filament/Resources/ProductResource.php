4ef13e45bceccab2de32f611da33394331b7d5cc
Vedat Kurtay <vedatkurtay@gmail.com>
2023-04-10 13:27:27 +0300

    Added multiselect for products

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -39,6 +39,8 @@ class ProductResource extends Resource
                 Forms\Components\TextInput::make('slug')->required(),
                 Forms\Components\TextInput::make('price')->required()->rules('numeric'),
                 Forms\Components\FileUpload::make('image')->image(),
+                Forms\Components\Select::make('tags')
+                ->relationship('tags', 'name')->multiple()
             ]);
     }
 
